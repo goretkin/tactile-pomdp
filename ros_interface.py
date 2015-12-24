481a2e14e3c976e2ff0817a8ddc3f1b0d8882400
goretkin <gustavo.goretkin@gmail.com>
2015-12-23 22:33:28 -0500

    add ros interface for force torque measurement

--- ros_interface.py
+++ ros_interface.py
@@ -4,8 +4,10 @@ import tf
 import rosgraph_msgs.msg as rosgraph_msg
 import visualization_msgs.msg as viz_msg
 import geometry_msgs.msg as geom_msg
+import std_msgs.msg as std_msg
 
 import numpy as np
+
 
 def init_publisher_node():
     return rospy.init_node("box2d_python_simulation")
@@ -24,6 +26,7 @@ def planar_shape_to_point3(shape):
     shape3.append(shape3[0])
     return shape3
 
+
 class DomainPublisher(object):
     def __init__(self, domain, controller=None):
         self.domain = domain
@@ -31,13 +34,17 @@ class DomainPublisher(object):
 
         self.clock_publisher = rospy.Publisher("/clock", rosgraph_msg.Clock, queue_size=1)
         self.viz_markers_publisher = rospy.Publisher("/ggrdf/fixtures", viz_msg.Marker, queue_size=5)
+        self.forcetorque_publisher = rospy.Publisher("/force_torque", geom_msg.WrenchStamped, queue_size=1)
+
         self.tf_broadcaster = tf.TransformBroadcaster()
 
         self.world_frame_name = "domain_base"
 
+
     def get_domain_sim_time(self):
         time_in_seconds = self.domain.simtime
         return rospy.Time.from_seconds(time_in_seconds)
+
 
     def publish_simtime(self):
         msg = rosgraph_msg.Clock(self.get_domain_sim_time())
@@ -128,9 +135,31 @@ class DomainPublisher(object):
         self.viz_markers_publisher.publish(marker)
 
 
+    def publish_forcetorque(self):
+        ft_w = self.domain.forcetorque_measurement
+        f_w = ft_w[0:2]
+        # this is ft measured in the physics simulation frame.
+        # however, in real life, the sensor is mounted on the grasp frame.
+        # so project the force into the rotated frame.
+
+        a = self.domain.dynamics.grasp_body.angle
+        m = tf.transformations.rotation_matrix(-a, [0, 0, 1])[0:2,0:2]
+        f_g = np.dot(m, f_w)
+
+        f = geom_msg.Vector3(f_g[0], f_g[1], 0)
+        t = geom_msg.Vector3(0, 0, ft_w[2])
+
+        ws = geom_msg.WrenchStamped(
+            std_msg.Header(None, self.get_domain_sim_time(), "grasp"), 
+            geom_msg.Wrench(f,t))
+
+        self.forcetorque_publisher.publish(ws)
+
+
     def publish(self):
         self.publish_simtime()
         self.publish_tf()
+        self.publish_forcetorque()
         self.publish_fixtures_shape()
 
 
