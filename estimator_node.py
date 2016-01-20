3a3e84f22aa933d7c85b3a11d347d797c9ea1c2b
goretkin <gustavo.goretkin@gmail.com>
2016-01-20 00:12:14 -0500

    fix many coordinate errors.

--- estimator_node.py
+++ estimator_node.py
@@ -19,6 +19,27 @@ def tf_to_mat(tf_transform):
     t, o = tf_transform
     return tf.transformations.translation_matrix(t).dot(tf.transformations.quaternion_matrix(o))
 
+def mat_to_tf(mat):
+    translation = tf.transformations.translation_from_matrix(mat)
+    orientation = tf.transformations.quaternion_from_matrix(mat)
+
+    return (translation, orientation)
+
+
+def world_grasp_to_relative(grasp_old, grasp_new):
+    inv = tf.transformations.inverse_matrix
+    # take two poses (tf format) expressed in a base frame
+    # express the transform between the two poses in the frame attached to the first pose
+    # return that transform in tf format.
+    displacement_in_old = inv(
+        tf_to_mat(grasp_old)).dot(
+        tf_to_mat(grasp_new))
+
+    translation = tf.transformations.translation_from_matrix(displacement_in_old)
+    orientation = tf.transformations.quaternion_from_matrix(displacement_in_old)
+
+    return (translation, orientation)
+
 
 class AggregateObservation(object):
     def __init__(self):
@@ -78,27 +99,19 @@ class AggregateObservation(object):
             # could consider using lookupTransformFull
 
             if self.last_grasp_pose_in_world_frame is not None and self.grasp_pose_in_world_frame is not None:
-                displacement_in_world = tf.transformations.inverse_matrix(
-                    tf_to_mat(self.last_grasp_pose_in_world_frame)).dot(
-                    tf_to_mat(self.grasp_pose_in_world_frame))
+                displacement_in_old = world_grasp_to_relative(
+                    self.last_grasp_pose_in_world_frame,
+                    self.grasp_pose_in_world_frame,
+                    )
 
-                # we want to convert the displacement expressed in world to displacement expressed in grasp_{last_time}
-                transformer = tf.transformations.quaternion_matrix(self.last_grasp_pose_in_world_frame[1])
-                displacement_in_grasp_last_time = tf.transformations.inverse_matrix(transformer).dot(displacement_in_world)
-
-                translation = tf.transformations.translation_from_matrix(displacement_in_grasp_last_time)
-                orientation = tf.transformations.quaternion_from_matrix(displacement_in_grasp_last_time)
-
-                #angle = tf.transformations.euler_from_quaternion(orientation, axes='sxyz')[2]
+                translation, orientation = displacement_in_old
 
                 #TODO this assumes that x,y axes of world and grasp are coplanar
                 angle = tf.transformations.euler_from_quaternion(
-                            tf.transformations.quaternion_from_matrix(
-                                displacement_in_world),
+                            orientation,
                             axes='sxyz'
                         )[2]
 
-                angle = - angle # ??? this is what looks right from debugging.
                 self.displacement_observation_2d = (translation[0], translation[1], angle)
 
             if self.update_cb is not None:
@@ -118,13 +131,6 @@ class AggregateObservation(object):
             self.action_msg = None
             self.force_torque_msg = None
 
-
-
-ao = AggregateObservation()
-
-belief_publisher = rospy.Publisher("/estimator_belief", std_msg.String, queue_size=10, latch=True)
-
-
 state_space = estimator_state_space.StateSpace()
 Belief = estimator_belief.belief_factory(state_space)
 estimator_object = estimator.Estimator(state_space)
@@ -132,39 +138,65 @@ obs_act_update = estimator_object.obs_act_update
 GuardedVelocity = estimator_transition_model.guarded_velocity_factory(state_space, Belief)
 
 
-class BeliefHolderSingleton(object):
-    current_belief = Belief()
-    @staticmethod
-    def reset():
-        # susceptible to race conditions
-        BeliefHolderSingleton.current_belief.clear()
-        for state in state_space.states:
-            BeliefHolderSingleton.current_belief[state] = 1.0
+class RecursiveEstimator(object):
+        def __init__(self):
+            self.current_belief = Belief()
+            self.reset()
+            self.condition_force_observation = True
+            self.condition_displacement_observation = True
 
-        BeliefHolderSingleton.current_belief.normalize()
+        def reset(self):
+            # susceptible to race conditions
+            self.current_belief.clear()
+            for state in state_space.states:
+                self.current_belief[state] = 1.0
+
+            self.current_belief.normalize()
+
+        def update_cb(self, displacement_obs, duration, force_obs, action_twist):
+            action_v = action_twist[0]
+            force_x = force_obs[0] if self.condition_force_observation else None
+            # the robot spits out a displacement observation in the gripper frame.
+            # it needs to get a sign flip, because the jig moves in the opposite direction
+            # the sign flip should be inside the obs_act_update in the future.
+            displacement_x = -displacement_obs[0] if self.condition_displacement_observation else None
+
+            duration_s = duration.to_sec()
+
+            time_before_update = time.time()
+
+            action = GuardedVelocity(action_v)
+            action.void_probability = 0.0 # void is fully absorbing
+
+            # this is just to debug. show the belief update before conditioning on observations
+            self.current_belief_prior = obs_act_update(
+                self.current_belief,
+                None,
+                None,
+                action,
+                duration_s)
+
+            self.current_belief = obs_act_update(
+                self.current_belief,
+                displacement_x,
+                force_x,
+                action,
+                duration_s)
+
+            time_after_update = time.time()
 
 
-belief_publisher.publish(std_msg.String(BeliefHolderSingleton.current_belief.to_json_string())) #initial belief
 
-def cb(displacement_obs, duration, force_obs, action_twist):
-    action_v = action_twist[0]
-    force_x = force_obs[0]
-    displacement_x = displacement_obs[0]
-    duration_s = duration.to_sec()
+            #rospy.loginfo(("Belief Update Time: %s ms"%(1000*(time_after_update-time_before_update)))
+            belief_publisher.publish(std_msg.String(self.current_belief.to_json_string()))
+            prior_belief_publisher.publish(std_msg.String(self.current_belief_prior.to_json_string()))
 
-    time_before_update = time.time()
+ao = AggregateObservation()
+re = RecursiveEstimator()
 
-    BeliefHolderSingleton.current_belief = obs_act_update(
-        BeliefHolderSingleton.current_belief,
-        displacement_x, 
-        force_x, 
-        GuardedVelocity(action_v),
-        duration_s)
+belief_publisher = rospy.Publisher("/estimator_belief", std_msg.String, queue_size=10, latch=True)
+prior_belief_publisher = rospy.Publisher("/estimator_prior_belief", std_msg.String, queue_size=10, latch=True)
 
-    time_after_update = time.time()
+belief_publisher.publish(std_msg.String(re.current_belief.to_json_string())) #initial belief
 
-    #rospy.loginfo(("Belief Update Time: %s ms"%(1000*(time_after_update-time_before_update)))
-    belief_publisher.publish(std_msg.String(BeliefHolderSingleton.current_belief.to_json_string()))
-
-BeliefHolderSingleton.reset()
-ao.update_cb = cb
+ao.update_cb = re.update_cb
